7b11d66f80a0b8c11a019df52081cd3dd665bedd
Natnael Chala <natnaelchala10@gmail.com>
2024-11-05 11:55:09 -0800

    f

--- app.js
+++ app.js
@@ -1,47 +1,20 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const os = require("os");
+// console.log(os)
 
-// Middleware to parse form data
-app.use(express.urlencoded({ extended: true }));
+const frememo = os.freemem()
+console.log(frememo)
 
-// In-memory store for todos (can be replaced with a database later)
-let todos = [
-  { id: 1, task: 'Learn Node.js' },
-  { id: 2, task: 'Build a simple project' }
-];
+const platform = os.platform()
+console.log(platform)
 
-// Route to display all todos
-app.get('/', (req, res) => {
-  let todoListHtml = '<h1>Todo List</h1><ul>';
-  todos.forEach(todo => {
-    todoListHtml += `<li>${todo.task} <a href="/delete/${todo.id}">Delete</a></li>`;
-  });
-  todoListHtml += '</ul>';
-  todoListHtml += `
-    <form action="/add" method="POST">
-      <input type="text" name="task" placeholder="New Todo" required>
-      <button type="submit">Add Todo</button>
-    </form>
-  `;
-  res.send(todoListHtml);
-});
+const fs = require("fs")
 
-// Route to add a new todo
-app.post('/add', (req, res) => {
-  const newTodo = { id: todos.length + 1, task: req.body.task };
-  todos.push(newTodo);
-  res.redirect('/');
-});
+fs.readFile("./abebe.txt", 'utf8', (err, data)=>{
+if(err) {
+    console.log(err)
+    return 
+}
+console.log(data)
+}
+)
 
-// Route to delete a todo
-app.get('/delete/:id', (req, res) => {
-  const todoId = parseInt(req.params.id);
-  todos = todos.filter(todo => todo.id !== todoId);
-  res.redirect('/');
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
