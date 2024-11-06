a0aa264ba4e946dd1bad3afe01a0e704cdc4676f
Natnael Chala <natnaelchala10@gmail.com>
2024-11-06 11:30:57 -0800

    d

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
-const os = require("os");
-// console.log(os)
+const http = require("http");   
+const fs = require("fs");   
+const url = require("url");   
+const mimetypelookup = require("mime-types").lookup;   
 
-const frememo = os.freemem()
-console.log(frememo)
+const server = http.createServer(function(req, res) {
+    const parsedUrl = url.parse(req.url, true);
+    const file_path = parsedUrl.pathname;  // Use pathname, not the full path
+    
 
-const platform = os.platform()
-console.log(platform)
 
-const fs = require("fs")
+    if (file_path === '/abebe.html') {
+        // Construct the full path to the file
+        var rfilename = __dirname + file_path;
+        
+        console.log(rfilename);
+        
+        fs.readFile(rfilename, function(err, data){
+            if(err){
+                
+                res.writeHead(404)
+                res.end();
+            }
+            else{
+                res.writeHead(200, {'Content-Type': 'text/html'});  // Set proper content type
+                res.end(data);
+            }
+        })    
+        
+    }
+    else if(file_path === '/'){
+        var rfilename = __dirname + '/apple/apple.html';
+        fs.readFile(rfilename, function(err, data){
+            if(err){
+                
+                res.writeHead(404)
+                res.end();
+            }
+            else{
+                res.writeHead(200, {'Content-Type': 'text/html'});  // Set proper content type
+                res.end(data);
+            }
+        })    
+    
+    }
 
-fs.readFile("./abebe.txt", 'utf8', (err, data)=>{
-if(err) {
-    console.log(err)
-    return 
-}
-console.log(data)
-}
-)
-
+     else {
+        // For other requests, send a generic message
+        res.writeHead(200, {'Content-Type': 'text/plain'});  // Set proper content type
+        res.end("Hey there");
+    }
+});
+const port = 1234
+server.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+});
